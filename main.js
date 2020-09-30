389defc006a839251ded41ef85d198c7e0f23370
coyoats <71328534+coyoats@users.noreply.github.com>
2020-09-29 20:22:51 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,54 @@
-window.onload = function() {
-    var address = "0x00000000000000000000000000000000f00dbaBE"
-    var moonbalance = 0
-    if (typeof web3 !== 'undefined') {
-      web3 = new Web3(web3.currentProvider);
-  
-    } else {
-      web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io"));
-    }
-   
+f (typeof web3 !== 'undefined') {
+    web3 = new Web3(web3.currentProvider);
 
-    console.log(web3.version);
-    if (typeof(window.ethereum) === "undefined"){
-        nometamaskwarn()
-    }else{
-        ethereum.on('accountsChanged', function (accounts) {
-            if (accounts[0]===undefined){
-                disconnected()
-            }
-            else{
-                updateBalance(accounts[0])
-                
-            }
-        })
-    }
-
+  } else {
+    web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io"));
   }
-  
-}
+
+function attemptConnect() {
+      try {
+          ethereum.request({
+              method: 'eth_requestAccounts'
+          }).then(function(res) {
+              updateBalance(res[0]);
+              update();
+          });
+          web3 = new Web3(web3.currentProvider);
+
+      } catch (error) {
+          document.getElementById("status").innerHTML = "Connection fail, try again";
+      }
+  }
+
+  if (window.ethereum === undefined) {
+      document.getElementById("con").hidden=true;
+      document.getElementById("status").innerHTML = "Please install Metamask to continue";
+  } else {
+      if (window.ethereum.networkVersion==="4"){
+      setInterval(function() {
+      update();
+      }, 3000);
+      ethereum.on('accountsChanged', function(accounts) {
+          console.log(accounts[0])
+          if (accounts[0] === undefined) {
+              document.getElementById("con").hidden=false;
+              document.getElementById("reset").hidden = true;
+              document.getElementById("claim").hidden = true;
+              document.getElementById("status").innerHTML = "Wallet disconnected";
+          } else {
+              updateBalance(accounts[0]);
+          }
+      });
+      }
+      else{
+          document.getElementById("con").hidden=true;
+
+          document.getElementById("status").innerHTML = "Wrong network! Please set network to Rinkeby in Metamask";
+
+
+      }
+  }
+  }
 
 var moonAddress = "0xdf82c9014f127243ce1305dfe54151647d74b27a"
 var bidAddress = "0x2d28f511bac4d8692dfaafd517a4c916c946828b"
@@ -253,7 +275,7 @@ ercabi = JSON.parse(`[
             "type": "event"
         }
     ]`)
-    
+
 boardabi = JSON.parse(`[{"constant":false,"inputs":[{"name":"newMessage","type":"string"},{"name":"newPrice","type":"uint256"}],"name":"set","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"price","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"summoner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"message","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"collect","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_summoner","type":"address"},{"name":"_token","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]`)
 
 var moonContract = new web3.eth.Contract(ercabi, moonAddress, {from:add, gasPrice:1000000000})
